d3b83383a526d82e0a042fcc8b28ef7931460cd6
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-21 23:31:59 +0500

    add env.ts

---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import 'dotenv-flow/config';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  PORT: z.coerce.number().default(4001),
+  DATABASE_URL: z.string().nonempty(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.log('Environment variables validation failed: ', parsedEnv.error.issues);
+  throw new Error('There is an error with the environment variables. ');
+}
+
+export const env = parsedEnv.data;
+
+export type ENV = z.infer<typeof envSchema>;
+
+declare global {
+  namespace NodeJS {
+    // @ts-ignore
+    interface ProcessEnv extends ENV {}
+  }
+}
