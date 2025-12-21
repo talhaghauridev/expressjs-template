ff8989ea5e15fd5658f547be6583360ef97d6133
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-21 23:33:35 +0500

    update

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import errorMiddleware from '@/middlewares/error.middleware';
+import { config } from '@/config/app.config';
+
+const app = express();
+
+// Middleware
+app.use(helmet(config.helemt));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(compression(config.compression));
+app.use(cors(config.cors));
+app.use(morgan('dev'));
+
+app.get('/', async (req, res) => {
+  return res.status(200).json({
+    message: 'Server is running',
+    success: true,
+  });
+});
+
+app.all('/*splat', (req, res) => {
+  return res.status(404).json({
+    message: 'Route not found',
+    success: false,
+  });
+});
+
+app.use(errorMiddleware);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const name_talha = 'hello'; // → ESLint error: 'unused' is defined but never used
-console.log(name_talha); // → Warning/Error: Unexpected console statement
-function bad(param: any) {
-  console.log({ param });
-} // → Error: Avoid using 'any'
-bad({});
+import app from './app';
+import { env } from './env';
 
-const fine = 'Talha';
+const server = app.listen(env.PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${env.PORT} in ${env.NODE_ENV.toUpperCase()} mode`
+  );
+});
+
+const shutdown = (signal: string) => {
+  console.log(`\n${signal} received: closing server...`);
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+export default app;
