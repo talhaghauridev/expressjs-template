15c699b0b997e555ee1fd89e389c8d93b804b445
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-21 21:29:31 +0500

    update

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const name_talha = 'hello'; // → ESLint error: 'unused' is defined but never used
+console.log(name_talha); // → Warning/Error: Unexpected console statement
+function bad(param: any) {
+  console.log({ param });
+} // → Error: Avoid using 'any'
+bad({});
