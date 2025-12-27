ecc466abe1dd897f338b248c4954fe2575951a1d
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-27 20:26:03 +0500

    add users.repository.ts

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,59 @@
+import { db } from '@/database/db';
+import { users, type InsertUser, type User } from '@/database/schema';
+import { SelectFields } from '@/types';
+import { buildReturning, normalizeSelect } from '@/utils/repository-helpers';
+import { eq } from 'drizzle-orm';
+
+export class UserRepository {
+  static async create(data: InsertUser, select?: SelectFields<User>) {
+    const [user] = await db.insert(users).values(data).returning(buildReturning(users, select));
+    return user;
+  }
+
+  static async findById(id: string, select?: SelectFields<User>) {
+    return await db.query.users.findFirst({
+      where: eq(users.id, id),
+      columns: normalizeSelect(select),
+    });
+  }
+
+  static async findByEmail(email: string, select?: SelectFields<User>) {
+    return await db.query.users.findFirst({
+      where: eq(users.email, email),
+      columns: normalizeSelect(select),
+    });
+  }
+
+  static async findByIdWithLocations(id: string, select?: SelectFields<User>) {
+    return await db.query.users.findFirst({
+      where: eq(users.id, id),
+      columns: normalizeSelect(select),
+      with: {
+        locations: true,
+      },
+    });
+  }
+
+  static async findByEmailWithLocations(email: string, select?: SelectFields<User>) {
+    return await db.query.users.findFirst({
+      where: eq(users.email, email),
+      columns: normalizeSelect(select),
+      with: {
+        locations: true,
+      },
+    });
+  }
+
+  static async update(id: string, data: Partial<InsertUser>, select?: SelectFields<User>) {
+    const [updated] = await db
+      .update(users)
+      .set({ ...data, updatedAt: new Date() })
+      .where(eq(users.id, id))
+      .returning(buildReturning(users, select));
+    return updated;
+  }
+
+  static async delete(id: string): Promise<void> {
+    await db.delete(users).where(eq(users.id, id));
+  }
+}
