88096b7923d57ccc236a99643939559dd2bfb2af
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-27 16:44:17 +0500

    update

--- src/database/migrations/0000_lumpy_shadowcat.sql
+++ src/database/migrations/0000_lumpy_shadowcat.sql
@@ -3,6 +3,20 @@ CREATE TYPE "public"."user_role" AS ENUM('admin', 'user');
 CREATE TYPE "public"."platform" AS ENUM('web', 'mobile');
 CREATE TYPE "public"."verification_type" AS ENUM('email', 'password_reset');
 CREATE TYPE "public"."location_type" AS ENUM('registration', 'last_login');
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"email" varchar(100) NOT NULL,
+	"password" varchar(60),
+	"provider_id" varchar(255),
+	"provider" "auth_provider" DEFAULT 'custom' NOT NULL,
+	"role" "user_role" DEFAULT 'user' NOT NULL,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"profile_completed" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+
 CREATE TABLE "verifications" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
@@ -27,28 +41,9 @@ CREATE TABLE "user_locations" (
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 
-ALTER TABLE "users" DROP CONSTRAINT "users_email_unique";
-ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE uuid;
-ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
-ALTER TABLE "users" ALTER COLUMN "email" SET DATA TYPE varchar(100);
-ALTER TABLE "users" ALTER COLUMN "password" SET DATA TYPE varchar(60);
-ALTER TABLE "users" ALTER COLUMN "password" DROP NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "name" SET DATA TYPE varchar(100);
-ALTER TABLE "users" ALTER COLUMN "name" SET NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "created_at" SET DATA TYPE timestamp with time zone;
-ALTER TABLE "users" ALTER COLUMN "created_at" SET DEFAULT now();
-ALTER TABLE "users" ALTER COLUMN "created_at" SET NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "updated_at" SET DATA TYPE timestamp with time zone;
-ALTER TABLE "users" ALTER COLUMN "updated_at" SET DEFAULT now();
-ALTER TABLE "users" ALTER COLUMN "updated_at" SET NOT NULL;
-ALTER TABLE "users" ADD COLUMN "provider_id" varchar(255);
-ALTER TABLE "users" ADD COLUMN "provider" "auth_provider" DEFAULT 'custom' NOT NULL;
-ALTER TABLE "users" ADD COLUMN "role" "user_role" DEFAULT 'user' NOT NULL;
-ALTER TABLE "users" ADD COLUMN "is_verified" boolean DEFAULT false NOT NULL;
-ALTER TABLE "users" ADD COLUMN "profile_completed" boolean DEFAULT false NOT NULL;
 ALTER TABLE "verifications" ADD CONSTRAINT "verifications_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
 ALTER TABLE "user_locations" ADD CONSTRAINT "user_locations_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
-CREATE INDEX "user_type_platform_idx" ON "verifications" USING btree ("user_id","type","platform");
 CREATE UNIQUE INDEX "email_idx" ON "users" USING btree ("email");
 CREATE UNIQUE INDEX "provider_user_idx" ON "users" USING btree ("provider","provider_id");
-ALTER TABLE "users" DROP COLUMN "is_active";
+CREATE INDEX "user_type_platform_idx" ON "verifications" USING btree ("user_id","type","platform");
+CREATE INDEX "user_id_type_idx" ON "user_locations" USING btree ("user_id","type");
