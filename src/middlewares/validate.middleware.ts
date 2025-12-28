4d369b0eb2826fde80d4ef8c4c2d927f317815ee
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-28 23:22:08 +0500

    add validate.middleware.ts

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodObject, ZodError } from 'zod';
+import ApiError from '@/utils/api-error';
+
+interface ValidationSchema {
+  body?: ZodObject;
+  query?: ZodObject;
+  params?: ZodObject;
+  headers?: ZodObject;
+}
+
+export const validate = (schema: ValidationSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (schema.body) {
+        req.body = schema.body.parse(req.body);
+      }
+
+      if (schema.query) {
+        req.query = schema.query.parse(req.query) as any;
+      }
+
+      if (schema.params) {
+        req.params = schema.params.parse(req.params) as any;
+      }
+
+      if (schema.headers) {
+        req.headers = schema.headers.parse(req.headers) as any;
+      }
+
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessage = error.issues.map((err) => err.message).join(', ');
+        return next(ApiError.badRequest(errorMessage));
+      }
+
+      next(error);
+    }
+  };
+};
