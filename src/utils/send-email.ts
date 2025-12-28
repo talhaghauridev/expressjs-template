1c7d4b6b9d56b061ddca6383648a1825c567ca7b
Talha Ghauri <talhaghauridev@gmail.com>
2025-12-28 19:02:37 +0500

    add send-email.ts

---
+++ src/utils/send-email.ts
@@ -0,0 +1,31 @@
+import nodemailer, { SendMailOptions } from 'nodemailer';
+import { env } from '@/env';
+import ApiError from '@/utils/api-error';
+import logger from './logger';
+
+export async function sendEmail(options: SendMailOptions): Promise<any> {
+  try {
+    const transporter = nodemailer.createTransport({
+      secure: true,
+      service: env.SMTP_SERVICE,
+      auth: {
+        user: env.SMTP_MAIL,
+        pass: env.SMTP_PASSWORD,
+      },
+    });
+
+    await transporter.verify();
+
+    const mailOption: SendMailOptions = {
+      from: env.SMTP_MAIL,
+      ...options,
+    };
+
+    const info = await transporter.sendMail(mailOption);
+    logger.info('Email sent: %s', info.messageId);
+    return info;
+  } catch (error: any) {
+    logger.error('Error sending email:', error);
+    throw ApiError.badRequest(error.message);
+  }
+}
